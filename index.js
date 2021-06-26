3e6359a32f58dc012ed81c75447679ca2f52b643
joalvarado2 <jonathanalva92@gmail.com>
2021-06-25 19:32:11 -0500

    mejorando estilos y logica

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
+// llammamos nuestros elementos del DOM es decir del navegador
+const cuadros = document.querySelectorAll("button");
+const parrafo = document.querySelector("p");
+
+// por cada boton que tenemos escuchamos un envento y le asignamos una funcion
+cuadros.forEach((e, i) => e.addEventListener("click", (e) => oprimirBtn(e, i)));
+
 let contarTurno = 0;
 const posicion = [];
 
+// cada vez que se oprime  un boton le asignamos un valor en texto y un valor en color
 const oprimirBtn = (e, i) => {
   contarTurno++;
   const btn = e.target;
-  const color = contarTurno % 2 ? "salmon" : "blue";
+  const text = contarTurno % 2 ? "X" : "O";
+  const color = contarTurno % 2 ? "#29b33c" : "#007bff";
+  btn.innerHTML = text;
   btn.style.backgroundColor = color;
   posicion[i] = color;
-  if (Ganador()) {
-    alert(`el jugador de color ${color} ha ganado`);
-    location.reload();
+  if (ganador()) {
+    parrafo.innerHTML = `
+      Felicitaciones a ganado el jugador con la letra ${text}<br/>
+      Oprime reset para comenzar una nueva partida  `;
   }
 };
 
-// const limpiar = () => {
-    
-// }
-
-const Ganador = () => {
+// funcion que tiene la logica para  sacar el ganador hacemos comparaciones en nuestro array de elementos
+const ganador = () => {
   if (posicion[0] == posicion[1] && posicion[0] == posicion[2] && posicion[0]) {
     return true;
   } else if (
@@ -65,7 +73,3 @@ const Ganador = () => {
   }
   return false;
 };
-
-document
-  .querySelectorAll("button")
-  .forEach((e, i) => e.addEventListener("click", (e) => oprimirBtn(e, i)));
